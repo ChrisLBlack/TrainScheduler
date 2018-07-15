87cf610534e4e9ca433b107b619cbe9bde7cacb0
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-07-15 12:03:43 -0500

    adding js

---
+++ assets/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function (){
+
+     // Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyByojJ1gn5MKkv7TCfrvsyALGuCQT2GHb4",
+    authDomain: "trainscheduler-3bb15.firebaseapp.com",
+    databaseURL: "https://trainscheduler-3bb15.firebaseio.com",
+    projectId: "trainscheduler-3bb15",
+    storageBucket: "",
+    messagingSenderId: "351425985615"
+    };
+    firebase.initializeApp(config);
+
+    var trainName = "";
+    var destName = "";
+    var trainTime = 0;
+    var freqTime = 0;
+
+
+    $("#add-train-btn").on("click", function (event) {
+        
+        event.preventDefault();
+
+        trainName = $('#train-name-input').val().trim();
+        destName = $('#dest-input').val().trim();
+        trainTime = $('#train-time-input').val().trim();
+        freqTime = $('#freq-input').val().trim();
+    
+
+        firebase.database().ref().push({
+            name: trainName,
+            dest: destName,
+            time: trainTime,
+            freq: freqTime
+        })
+
+       $("#train-name-input").val("");
+       $('#dest-input').val("");
+       $('#train-time-input').val("");
+       $('#freq-input').val("");
+
+    });
+
+    firebase.database().ref().limitToLast(10).on("child_added", function(snapshot){
+       
+        $('tbody').append($(`<tr><td>${snapshot.val().name}</td><td>${snapshot.val().dest}</td>
+        <td>${snapshot.val().time}</td><td>${snapshot.val().freq}</td></tr>`));
+        
+        
+    })
+
+
+
+});
